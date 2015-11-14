b206d57cfbcb4e5e6d8e48cf8537f30283c945b8
Jianfeng Chen <jchen37@login02.hpc.ncsu.edu>
2015-11-14 17:28:42 -0500

    reorganize files

--- calc_repu/bash_update_movie_grade.sh
+++ calc_repu/bash_update_movie_grade.sh
@@ -5,5 +5,5 @@ rm err/*
 rm temgrade/*
 
 foreach VAR (0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17)
-  bsub -W 200 -n 4 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J mpiexec -n 4 $PYTHON  te.py run $VAR
+  bsub -W 200 -n 4 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J mpiexec -n 4 $PYTHON  getMovieGrade.py run $VAR
 end
--- calc_repu/create_movie_grade.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-f = open('movie_ground','w')
-ground = [1]*17771
-pickle.dump(ground,f)
