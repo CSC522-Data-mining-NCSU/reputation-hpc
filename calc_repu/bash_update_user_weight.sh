369462149089edccc5bb7c67058433c58ec727ba
Jianfeng Chen <jchen37@login02.hpc.ncsu.edu>
2015-11-15 13:01:13 -0500

    add leniency

--- calc_repu/bash_update_user_weight.sh
+++ calc_repu/bash_update_user_weight.sh
@@ -3,8 +3,9 @@
 rm out/*
 rm err/*
 rm temweight/*
+rm temleniency/*
 
-foreach VAR(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48)
+foreach VAR(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26)
 	bsub -W 2500 -n 4 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J mpiexec -n 4 $PYTHON deltaR.py run $VAR
 end
 
--- calc_repu/deltaR.py
+++ calc_repu/deltaR.py
@@ -1,72 +1,86 @@
 import pickle
 import csv
 import pdb
+import linecache
 from mpi4py import MPI
 from demos import cmd
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-oneProcessorWorkload = 2500
 
 with open('movie_grade.list', 'rb') as f:
-	G = pickle.load(f)
-
-def fetchRating(user_id, movie_id):
-	rating = 0
-	with open('../training_set/mv_'+str(movie_id).zfill(7)+'.txt','r') as f:
-		reader = csv.reader(f)
-		reader.next()
-		for row in reader:
-			a = int(row[0])
-			if a == user_id:
-				rating = float(row[1])
-				break
-	return rating
-
-
-def startpoint(st):
-	r  = {}
-	um = open('user_movie.csv', 'rb')
-	ums = csv.reader(um)
-	next(ums) #ignore the first line
-	
-	for i,line in enumerate(ums):
-		if i < st: continue
-		user = int(line[0])
-		delta = 0
-		if line[1][-1]==',': # TODO BUG RESPONSE
-			#movies = map(int, line[1][:-1].split(','))
-			continue
-		else:
-			movies = map(int, line[1].split(','))
-		for m in movies:
-			delta += (G[m]-fetchRating(user,m))**2
-		delta = delta / len(movies)
-		print user, delta
-		r[str(user)] = float(delta)
-		if i >= st+oneProcessorWorkload-1:break
-	um.close()
-	return r
+	GG = pickle.load(f)
 
 def run(q):
 	processors = 4
-	st = int(q) * oneProcessorWorkload * processors
-	st += rank*oneProcessorWorkload + 1
-	r = startpoint(st)
+	urf = '../userRates/'+str(q)+'.csv'
+	totalline = 0
+	with open(urf,'rb') as ur:
+		urr = csv.reader(ur)
+		totalline = sum(1 for x in ur)
 
+	start = [0]*processors
+	with open(urf,'rb') as ur:
+		urr = csv.reader(ur)	
+		urr.next()
+		l = [int(float(x)/processors*totalline) for x in range(processors)]
+		for i,line in enumerate(urr):
+			if i in l:
+				start[l.index(i)] = int(line[0])
+	
+	with open(urf,'rb') as ur:
+		ww = {}
+		ll = {}
+		urr = csv.reader(ur)
+		urr.next()
+		lastu = 0
+		count = 0	
+		#weight = 0
+		leniency = 0
+		for line in urr:
+			u = int(line[0])
+			if u < start[rank]: continue
+			if rank < processors - 1:
+				if u >= start[rank+1]: break
+			if u != lastu:
+				if lastu != 0: #recording the results for lastu
+					#weight = weight/count
+					leniency = leniency/count
+					if leniency < -1 :leniency = -1
+					if leniency > 1: leniency = 1
+					ww[str(lastu)] = 1-abs(leniency)
+					ll[str(lastu)] = leniency
+					print lastu
+					#if abs(leniency) > 0.8: print lastu,leniency
+				lastu = u
+				count = 0
+				weight = 0
+				leniency = 0
+			m = int(line[1])
+			g = float(line[2])
+			G = GG[m]
+			count += 1
+			#weight = (g-G)**2
+			leniency += (g-G)/g
+			
 	if rank == 0:
 		for i in range(1,processors):
 			temp = comm.recv(source=i)
-			r.update(temp)
+			ww.update(temp[0])
+			ll.update(temp[1])
 		with open('temweight/'+str(q)+'_tem_weight','wb') as tew:
-			pickle.dump(r,tew)
+			pickle.dump(ww,tew)
+		with open('temleniency/'+str(q)+'_tem_leniency','wb') as tel:
+			pickle.dump(ll,tel)
 	else:
-		comm.send(r,dest=0)
+		ttt = [ww,ll]
+		comm.send(ttt,dest=0)
+
 
 if __name__ == '__main__':
 	eval(cmd())
-
+	#run(0)
 
 
 
